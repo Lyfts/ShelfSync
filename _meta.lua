d586475f5b53936d7957144d04a46f6a2a25c0d7
Lyft <127234178+Lyfts@users.noreply.github.com>
2026-08-19 17:34:49 +0200

    v0.2.5

--- _meta.lua
+++ _meta.lua
@@ -3,6 +3,6 @@ return {
   name = "storygraph",
   fullname = _("StoryGraph"),
   description = _([[Synchronize reading progress to app.thestorygraph.com]]),
-  version = "0.2.4",
+  version = "0.2.5",
   api_version = 1
 }
